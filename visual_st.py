1ae875b341018a41ee29e2a25f0d7ca4f5439bde
aaronbarreramartin <aaronbarreramartin@gmail.com>
2025-12-06 17:56:43 +0100

    Update visual_st.py

--- visual_st.py
+++ visual_st.py
@@ -66,7 +66,7 @@ def mostrar_contadores():
 if __name__ == '__main__':
     st.set_page_config(page_title='Horario para perro', page_icon=':dog:')
     contraseña = st.secrets['contrasena']
-    col1, col2 = st.columns(2,1)
+    col1, col2 = st.columns((2,1))
     with col1:
         intro_cntrs = st.text_input('Introduce la contraseña', type='password')
     with col2:
@@ -97,6 +97,7 @@ if __name__ == '__main__':
             st.dataframe(df_registros, hide_index=True) 
         else:
             st.info('Vacío')
+
 
 
 
