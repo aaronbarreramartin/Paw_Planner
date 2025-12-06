e280405096f9c3e8ab2f77f823e346586964ae56
aaronbarreramartin <aaronbarreramartin@gmail.com>
2025-12-06 18:04:01 +0100

    Update visual_st.py

--- visual_st.py
+++ visual_st.py
@@ -67,10 +67,9 @@ if __name__ == '__main__':
     st.set_page_config(page_title='Horario para perro', page_icon=':dog:')
     contraseña = st.secrets['contrasena']
     col1, col2 = st.columns([2,1])
-    with col1:
-        intro_cntrs = st.text_input('', type='password')
-        st.text('Introduce la contraseña')
-    with col2:
+    with col1 and st.container:
+        intro_cntrs = st.text_input('Introduce la contraseña', type='password')
+    with col2 and st.container:
         invitado = st.button('Entrar como invitado')
     
     if intro_cntrs == contraseña or invitado:
@@ -98,6 +97,7 @@ if __name__ == '__main__':
             st.dataframe(df_registros, hide_index=True) 
         else:
             st.info('Vacío')
+
 
 
 
