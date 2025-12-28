ade8d1e40a16f4573b7905be684b1477c2479492
aaronbarreramartin <aaronbarreramartin@gmail.com>
2025-12-28 14:52:38 +0100

    Update visual_st.py

--- visual_st.py
+++ visual_st.py
@@ -93,10 +93,11 @@ if __name__ == '__main__':
         st.subheader('Registro:')
         df_registros = Hoja('Registro').df
         if not df_registros.empty:
-            df_registros.sort_values(by='fecha', ascending=False)   
+            df_registros = df_registros.sort_values(by='fecha', ascending=False)   
             st.dataframe(df_registros, hide_index=True) 
         else:
             st.info('Vacío')
+
 
 
 
